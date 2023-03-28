ab0c74533228e7673823b20792d04ce93af247a6
Lukas Jurk <lukas.jurk@tu-dresden.de>
2023-03-28 11:29:02 +0200

    replace i3blocks with polybar

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+BAR_NAME=main
+BAR_CONFIG=/home/$USER/.config/polybar/config.ini
+
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch on primary monitor
+MONITOR=$PRIMARY polybar --reload -c $BAR_CONFIG $BAR_NAME &
+sleep 1
+
+# Launch on all other monitors
+for m in $OTHERS; do
+ MONITOR=$m polybar --reload -c $BAR_CONFIG $BAR_NAME &
+done
