553fe23a7938405bd274b65b7ff46cd8aebe3a21
Lukas Jurk <lukas.jurk@tu-dresden.de>
2024-08-22 15:20:29 +0200

    feat(nvim): add GitGutterStageHunk

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -2,13 +2,14 @@ return {
     {
         "airblade/vim-gitgutter",
         config = function()
-            vim.keymap.set("n", "<leader>ga", ":GitGutterStageHunk<Enter>", {})
+            vim.keymap.set("n", "<leader>ga", ":GitGutterStageHunk<Enter>", { desc = "[g]it [a]dd hunk" })
+            vim.keymap.set("v", "<leader>ga", ":GitGutterStageHunk<Enter>", { desc = "[g]it [a]dd hunk" })
+            vim.keymap.set("n", "<leader>gu", ":GitGutterUndoHunk<Enter>", { desc = "[g]it [u]ndo hunk" })
         end,
     },
     {
         "tpope/vim-fugitive",
         config = function()
-            vim.keymap.set("n", "<leader>ga", ":GitGutterStageHunk<Enter>", {})
             vim.keymap.set("n", "<leader>gs", ":Git<Enter>", {})
             vim.keymap.set("n", "<leader>gc", ":Git commit<Enter>", {})
             vim.keymap.set("n", "<leader>gcn!", ":Git commit --no-edit --amend<Enter>", {})
