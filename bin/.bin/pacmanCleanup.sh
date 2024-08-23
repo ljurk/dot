abb52d2547a1fa3e28c2ca45442aff2da3e0e019
Lukas Jurk <lukas.jurk@tu-dresden.de>
2024-08-23 07:58:27 +0200

    feat(bin): remove provisioned packages from pacmanCleanup

--- bin/.bin/pacmanCleanup.sh
+++ bin/.bin/pacmanCleanup.sh
@@ -2,9 +2,34 @@
 
 # List explicitly installed packages
 explicit_packages=$(pacman -Qe | awk '{print $1}')
+pacman_packages_file="packages.txt"
+aur_packages_file="packages-aur.txt"
+
+temp_file=$(mktemp)
+sed -e '/^\s*#/d' -e '/^$/d' "$pacman_packages_file" > "$temp_file"
+sed -e '/^\s*#/d' -e '/^$/d' "$aur_packages_file" >> "$temp_file"
+
+# If the pacman packages file exists, get its content
+if [ -f "$temp_file" ]; then
+    saved_packages=$(<"$temp_file")
+else
+    saved_packages=""
+fi
+
+#echo "$saved_packages"
+
+# Loop through each explicitly installed package
+for package in $explicit_packages; do
+    # Check if the package is not in the saved list
+    if ! grep -q "^$package$" <<< "$saved_packages"; then
+        new_packages+="$package\n"
+        #echo "$package"
+    fi
+done
+
 
 # Display packages using fzf for selection
-selected_packages=$(echo "$explicit_packages" | fzf --prompt="Select packages to delete: " --multi --preview-window=right:50% --preview='pacman -Qi {}')
+selected_packages=$(printf "$new_packages" | fzf --prompt="Select packages to delete: " --multi --preview-window=right:50% --preview='pacman -Qi {}')
 
 # If no package selected, exit
 if [ -z "$selected_packages" ]; then
