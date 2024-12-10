d7df8d744fa0a44a9ad03197c227ec8d03c62b5f
Lukas Jurk <lukas.jurk@tu-dresden.de>
2024-12-10 11:31:47 +0100

    feat(polybar): only show polybar on active monitors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,7 @@ BAR_NAME=main
 BAR_CONFIG=/home/$USER/.config/polybar/config.ini
 
 PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
-OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | grep '[0-9]\+x[0-9]\+' | cut -d" " -f1)
 
 # Launch on primary monitor
 MONITOR=$PRIMARY polybar --reload -c $BAR_CONFIG $BAR_NAME &
