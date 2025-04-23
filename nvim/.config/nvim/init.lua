b3bc73a7312ba8fce125cc5599c4fce3caa37259
Lukas Jurk <lukas.jurk@tu-dresden.de>
2025-04-23 10:27:16 +0200

    feat(nvim): add custom command GitKeepFirstCommit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -45,3 +45,17 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
+
+vim.api.nvim_create_user_command("GitKeepFirstCommit", function()
+    local start = vim.fn.search("^# This is the commit message #2:", "n")
+    local finish = vim.fn.search("^# Please enter", "n")
+
+    if start > 0 and finish > start then
+        vim.cmd(string.format("%d,%dd", start - 1, finish - 1))
+    end
+
+    -- Remove remaining comment lines
+    vim.cmd([[silent! g/^#.*$/d]])
+    vim.cmd([[silent! g/^$/d]])
+    vim.cmd([[silent! wq]])
+end, {})
