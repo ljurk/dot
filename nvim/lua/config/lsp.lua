338767075bd91d0ab6fe873984fd252174d4840a
Lukas Jurk <lukas.jurk@tu-dresden.de>
2026-09-20 13:14:45 +0200

    feat(nvim): setup pyright

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -6,19 +6,19 @@
 -- This actually just enables the lsp servers.
 -- The configuration is found in the lsp folder inside the nvim config folder,
 -- so in ~.config/lsp/lua_ls.lua for lua_ls, for example.
-vim.lsp.enable({ "lua_ls", "golang", "ruff" })
+vim.lsp.enable({ "lua_ls", "golang", "ruff", "pyright" })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method("textDocument/completion") then
-            vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-            vim.keymap.set("i", "<C-Space>", function()
-                vim.lsp.completion.get()
-            end)
-        end
-    end,
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method("textDocument/completion") then
+      vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      vim.keymap.set("i", "<C-Space>", function()
+        vim.lsp.completion.get()
+      end)
+    end
+  end,
 })
 
 -- Diagnostics
